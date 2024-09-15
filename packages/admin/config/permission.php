fb2e3ac10087117a8443403cd72d726f9ee897ce
samimhamza <samim.hamza7@gmail.com>
2024-09-15 13:28:09 +0430

    permissions changed

--- packages/admin/config/permission.php
+++ packages/admin/config/permission.php
@@ -1,6 +1,5 @@
 <?php
 
-use Lunar\Admin\Models\Permission;
 use Lunar\Admin\Models\Role;
 
 return [
@@ -16,8 +15,7 @@ return [
          * `Spatie\Permission\Contracts\Permission` contract.
          */
 
-        'permission' => Permission::class,
-        // Spatie\Permission\Models\Permission::class,
+        'permission' => Spatie\Permission\Models\Permission::class,
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which

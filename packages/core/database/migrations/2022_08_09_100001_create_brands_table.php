14fb4aba3896247cd8618977aafaa28ac6d8c6d5
Author <actions@github.com>
2024-11-16 09:39:26 +0000

    chore: fix code style

--- packages/core/database/migrations/2022_08_09_100001_create_brands_table.php
+++ packages/core/database/migrations/2022_08_09_100001_create_brands_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create($this->prefix . 'brands', function (Blueprint $table) {
+        Schema::create($this->prefix.'brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -18,6 +18,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists($this->prefix . 'brands');
+        Schema::dropIfExists($this->prefix.'brands');
     }
 };

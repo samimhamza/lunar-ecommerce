eb493110d08ff20a891f46ac0371ce4153d311f4
samimhamza <samim.hamza7@gmail.com>
2024-12-09 12:27:18 +0430

    bugs fixed

--- packages/core/database/migrations/2021_07_29_100010_create_product_types_table.php
+++ packages/core/database/migrations/2021_07_29_100010_create_product_types_table.php
@@ -8,16 +8,16 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create($this->prefix.'product_types', function (Blueprint $table) {
+        Schema::create($this->prefix . 'product_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
-            $table->foreignId('tenant_id')->nullable()->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignUuid('tenant_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists($this->prefix.'product_types');
+        Schema::dropIfExists($this->prefix . 'product_types');
     }
 };

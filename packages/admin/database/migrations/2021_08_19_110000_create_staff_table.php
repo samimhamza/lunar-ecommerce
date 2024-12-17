bb17e47e7b1eac9cd48df0033f1c4e7bdec4a662
samimhamza <samim.hamza7@gmail.com>
2024-12-17 15:57:55 +0430

    emails changed to nullable

--- packages/admin/database/migrations/2021_08_19_110000_create_staff_table.php
+++ packages/admin/database/migrations/2021_08_19_110000_create_staff_table.php
@@ -8,14 +8,15 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create($this->prefix.'staff', function (Blueprint $table) {
+        Schema::create($this->prefix . 'staff', function (Blueprint $table) {
             $table->id();
             $table->boolean('admin')->default(false)->index();
             $table->string('firstname')->index();
             $table->string('lastname')->index();
             $table->string('phone');
+            $table->string('username')->nullable();
             $table->timestamp('phone_verified_at')->nullable();
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -24,11 +25,12 @@ return new class extends Migration
             $table->foreignUuid('tenant_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->unique(['email', 'tenant_id']);
             $table->unique(['phone', 'tenant_id']);
+            $table->unique(['username', 'tenant_id']);
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists($this->prefix.'staff');
+        Schema::dropIfExists($this->prefix . 'staff');
     }
 };
